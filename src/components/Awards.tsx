7de15aab9eebb25db6dd7d372f6fb06540d83efb
Kartavya ojha <91776415+kartavyaojha@users.noreply.github.com>
2025-09-03 15:35:05 +0530

    Update Awards.tsx

--- src/components/Awards.tsx
+++ src/components/Awards.tsx
@@ -32,7 +32,7 @@ const Awards = () => {
     },
     {
       title: "Introduction to Splunk",
-      organization: "Splunk",
+      organization: "sp",
       year: "2024",
       description: "Certified in leveraging Splunk for data analysis, SPL queries, and monitoring security logs.",
       icon: Award,
